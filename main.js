c7059194a4e631f2d02af68d6389e71beb7bfd6a
px106 <ultramatrix.will@gmail.com>
2015-10-17 10:27:31 +1300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var loadgame = function(){
+    var Name = prompt("Now that the game's loaded, welcome to GAIA! What's your name?");
+    var Answer = prompt("Alright, " + Name + "! What's 7 + 7?");
+    if (Answer === "14"){
+        
+    }
+};
+var points = "undefined";
+alert("Press OK to prompt game. This requires a tiny bit of waiting.");
+setTimeout(loadgame, 3000);
+
+
+
+
